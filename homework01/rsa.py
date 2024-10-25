721d0c0cf45fe9d920e26fdb8296374139c9365f
masha <lomakinamaria@bk.ru>
2024-10-25 21:13:17 +0300

    Реализована функция is_prime(n)

--- homework01/rsa.py
+++ homework01/rsa.py
@@ -12,7 +12,12 @@ def is_prime(n: int) -> bool:
     >>> is_prime(8)
     False
     """
-    # PUT YOUR CODE HERE
+    if n < 2:
+        return False
+    for i in range (2, int(n**0.5) + 1):
+        if n%i == 0:
+            return False
+    return True
     pass
 
 
